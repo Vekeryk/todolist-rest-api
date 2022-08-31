919c8a13edb40fe8cc42181ec06683c3f924896d
Денис Векерик <den.vekeryk@gmail.com>
2022-09-01 01:08:17 +0300

    Created required dto for UserController.

---
+++ src/main/java/com/softserve/itacademy/todolist/dto/user/UserCreateRequest.java
@@ -0,0 +1,39 @@
+package com.softserve.itacademy.todolist.dto.user;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Data
+@NoArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class UserCreateRequest {
+
+    @Pattern(regexp = "[A-Z][a-z]+",
+            message = "Must start with a capital letter followed by one or more lowercase letters")
+    String firstName;
+
+    @Pattern(regexp = "[A-Z][a-z]+",
+            message = "Must start with a capital letter followed by one or more lowercase letters")
+    String lastName;
+
+    @NotBlank
+    @Email
+    String email;
+
+    @NotBlank
+    @Pattern(regexp = "[A-Za-z\\d]{6,}",
+            message = "Must be minimum 6 symbols long, using digits and latin letters")
+    @Pattern(regexp = ".*\\d.*",
+            message = "Must contain at least one digit")
+    @Pattern(regexp = ".*[A-Z].*",
+            message = "Must contain at least one uppercase letter")
+    @Pattern(regexp = ".*[a-z].*",
+            message = "Must contain at least one lowercase letter")
+    String password;
+}
