3851b5eeb08f3a0c46f3d9e03b0f9acd974c1538
Денис Векерик <den.vekeryk@gmail.com>
2022-09-03 02:11:04 +0300

    Fixed sql query.

--- src/main/java/com/softserve/itacademy/todolist/repository/ToDoRepository.java
+++ src/main/java/com/softserve/itacademy/todolist/repository/ToDoRepository.java
@@ -7,13 +7,15 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface ToDoRepository extends JpaRepository<ToDo, Long> {
-    @Query(value = "select * " +
-                   "from todos\n" +
-                   "    where owner_id = :userId\n" +
-                   "union\n" +
-                   "select * " +
-                   "from todos t inner join todo_collaborator tc\n" +
-                   "    on t.id = tc.todo_id and tc." +
-                   "collaborator_id = :userId;", nativeQuery = true)
+    @Query(value = """
+        select id, title, created_at, owner_id
+        from todos
+        where owner_id = ?1
+        union
+        select id, title, created_at, owner_id
+        from todos
+        inner join todo_collaborator
+        on id = todo_id and collaborator_id = ?1
+        """, nativeQuery = true)
     List<ToDo> getByUserId(long userId);
 }
