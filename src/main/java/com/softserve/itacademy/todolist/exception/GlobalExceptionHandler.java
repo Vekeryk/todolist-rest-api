d0910b711d8955988571b2ebe5c0089e34e8d9a2
Денис Векерик <den.vekeryk@gmail.com>
2022-09-04 01:06:56 +0300

    Customised exception handling.

--- src/main/java/com/softserve/itacademy/todolist/exception/GlobalExceptionHandler.java
+++ src/main/java/com/softserve/itacademy/todolist/exception/GlobalExceptionHandler.java
@@ -1,59 +1,69 @@
 package com.softserve.itacademy.todolist.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.persistence.EntityNotFoundException;
 import javax.servlet.http.HttpServletRequest;
-import java.time.LocalDateTime;
-import java.util.LinkedHashMap;
+import javax.validation.ConstraintViolationException;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 @Slf4j
 @RestControllerAdvice
-public class GlobalExceptionHandler { // consider extending ResponseEntityExceptionHandler
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler { // consider extending ResponseEntityExceptionHandler
 
-    @ExceptionHandler
-    public ResponseEntity<?> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
-        Map<String, Object> body = new LinkedHashMap<>();
-        body.put("timestamp", LocalDateTime.now());
-        body.put("status", HttpStatus.BAD_REQUEST.value());
-
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         Map<String, List<String>> errors = ex.getBindingResult()
                 .getFieldErrors()
                 .stream()
                 .collect(Collectors.groupingBy(
                         FieldError::getField,
-                        Collectors.mapping(fieldError -> fieldError.getDefaultMessage(), Collectors.toList())
+                        Collectors.mapping(DefaultMessageSourceResolvable::getDefaultMessage, Collectors.toList())
                 ));
-        body.put("errors", errors);
-
-        return ResponseEntity.badRequest().body(body);
+        return ResponseEntity.badRequest().body(errors);
     }
 
     @ExceptionHandler
-    public ResponseEntity<?> handleNullEntityReferenceException(NullEntityReferenceException ex) {
-        // todo
+    public ResponseEntity<?> handleNullEntityReferenceException(NullEntityReferenceException ex, HttpServletRequest request) {
+        log.error("Error for '{} {}' failed with error: {} \n {}",
+                request.getMethod(), request.getRequestURL(), ex.getClass(), ex.getMessage());
         return ResponseEntity.badRequest().body(ex.getMessage());
     }
 
     @ExceptionHandler
     public ResponseEntity<?> handleEntityNotFoundException(EntityNotFoundException ex) {
-        // todo: add logging
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
     @ExceptionHandler
-    public ResponseEntity<?> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
-        return ResponseEntity.badRequest().body(ex.getMessage());
+    public ResponseEntity<?> handleBadCredentialsException(BadCredentialsException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getHttpInputMessage());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<?> handleConstraintViolationException(ConstraintViolationException ex, HttpServletRequest request) {
+        log.error("Database error for '{} {}' failed with error: {} \n {}",
+                request.getMethod(), request.getRequestURL(), ex.getClass(), ex.getConstraintViolations());
+        return ResponseEntity.internalServerError().body(ex.getMessage());
     }
 
     @ExceptionHandler
